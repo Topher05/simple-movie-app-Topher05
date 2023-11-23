fb3246429887b067a9749259c3dffe6b9221fb4d
EC2 Topher <89708367+Topher05@users.noreply.github.com>
2023-11-23 01:04:48 +0000

    Added a put method in main.py main.py can update movies using put movies.py now has a update model for updating movies issue #2

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,10 @@
 from typing import Union
 from fastapi import FastAPI
-from movies import my_movies, Movie
+from movies import my_movies, Movie, MovieNew, MovieUpdate
 
 app = FastAPI()
 
-print(my_movies)
+#print(my_movies)
 @app.get("/movies/{movie_id}")
 def get_movie_by_id(movie_id: str) -> Union[Movie,None]:
 	if movie_id in my_movies:
@@ -13,3 +13,18 @@ def get_movie_by_id(movie_id: str) -> Union[Movie,None]:
 			return movie
 
 	return None
+
+@app.put("/movies/{movie_id}", response_model=MovieUpdate)
+def update_movie(movie_id: str, update_movie: MovieUpdate) -> Union[Movie,None]:
+	if movie_id in my_movies:
+		movie = my_movies[movie_id]
+		if movie.id == movie_id:
+			my_update_movie = Movie(
+				id = movie_id,
+				name = update_movie.name,
+				cast = update_movie.cast
+			)
+			my_movies[movie_id] = my_update_movie
+			return my_movies[movie_id]
+	return None
+
